389ee058f22b27957c7688e2efe8e0f86c521de0
Silgho <110030849+Silgho@users.noreply.github.com>
2023-04-06 07:16:07 -0400

    A python code to determine whether the answer given by a user to a provided question the user has the correct answer or not

---
+++ deep.py
@@ -0,0 +1,10 @@
+def deep1():
+    print("Answer the following question.")
+    Q = input("The Great Question of Life, the Universe and Everything? (one word Answer).  :  ")
+    if Q .lower() in ["42", "forty two", "forty-two"]:
+        print("Yes")
+    else:
+        print("No")
+        
+            
+deep1()        
