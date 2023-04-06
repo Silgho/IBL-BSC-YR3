9baff1ed29e9eabd6fc5804621e50efaf56534e1
Silgho <110030849+Silgho@users.noreply.github.com>
2023-04-06 09:49:50 -0400

    While loop in python

---
+++ whileloops.py
@@ -0,0 +1,19 @@
+#printing multiple times without repeating the code:
+#for a junior engineer
+number_of_time = 3
+while number_of_time != 0 :
+    print("Hello World!")
+    number_of_time = number_of_time - 1
+    
+#for a middle engineer
+number_of_time = 1
+while number_of_time <= 3 :
+    print("Hello World!")
+    number_of_time = number_of_time + 1
+
+#for senior engineer
+number_of_time = 0
+while number_of_time < 3 :
+    print("Hello World!")
+    number_of_time += 1
+    
