3db2b6580d3ccca79d532689cd77260a779fd22f
Silgho <110030849+Silgho@users.noreply.github.com>
2023-04-06 09:49:18 -0400

    For Loop in python

---
+++ forloops.py
@@ -0,0 +1,18 @@
+#for a junior engineer
+for number_of_time in [0, 1, 2,]:
+    print("hello world!")
+    
+#for a middle-level engineer
+for number_of_time in range(3):
+    print("hello world!")
+    
+#for a senior engineer
+for _ in range(2):
+    print("hello world!")
+    
+#in a simple line
+print("hello world!" * 3)
+#in multiple lines [the end="" is to remove unnecessary space at the end]
+print("hello world!\n" * 3, end="")
+    
+    
